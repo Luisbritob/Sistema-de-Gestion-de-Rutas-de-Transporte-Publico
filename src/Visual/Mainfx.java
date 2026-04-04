f40d5a552fe79076c9fe53232a8abc14b5bbab97
Juan Diego <jdsz0001@ce.pucmm.edu.do>
2026-04-03 21:49:14 -0400

    Habemus Database

--- src/Visual/Mainfx.java
+++ src/Visual/Mainfx.java
@@ -4,12 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.nio.file.Paths;
 
 public class Mainfx extends Application {
 
    @Override
    public void start(Stage stage) throws Exception {
-      FXMLLoader loader = new FXMLLoader(getClass().getResource("/Recursos/MainView.fxml"));
+      FXMLLoader loader = new FXMLLoader(Paths.get("Recursos/MainView.fxml").toUri().toURL());
       Scene scene = new Scene(loader.load());
 
       stage.setTitle("RouteFinder");
